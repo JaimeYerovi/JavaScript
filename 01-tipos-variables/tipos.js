b87cfae699799019f2caebad3411971a117f5b51
JaimeYerovi <mito_yero@hotmail.com>
2018-04-17 14:55:45 -0500

    variables en javascript

---
+++ 01-tipos-variables/tipos.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jaime on 17/04/2018.
+ */
+
+
+var nombre = "23";
+var edad = 23;
+var fecha = new Date();
+
+console.log()
+
+if(nombre === edad){
+    console.log("verdadero")
+}else{
+    console.log("Falso")
+}
+
